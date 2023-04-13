03b24c208f085efd5a3e2419e052e66e46b625ec
YuenShanTsang <A00270323@mycambrian.ca>
2023-04-13 15:16:28 -0400

    Added style, night mode switch in action bar

--- app/src/main/java/com/example/dicerollingapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollingapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.dicerollingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.dicerollingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Init view binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    // Add night switch in menu of the action bar
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val item = menu.findItem(R.id.night_switch)
+        val switchView = item.actionView as Switch
+
+        // Set the switch state based on the current night mode
+        switchView.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        // Set a listener for changes to the switch state
+        switchView.setOnCheckedChangeListener { _, isChecked ->
+            val mode = if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
+
+        return true
+    }
+
+
+
 }
